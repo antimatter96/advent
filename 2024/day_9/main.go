4c0fc3228b3d674e7510b132bf3e0cda8dcf10cd
Arpit Jain <me.arpitjain09@gmail.com>
2024-12-09 14:36:22 +0800

    Day 9 P1

---
+++ 2024/day_9/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"github.com/antimatter96/advent/2024/common"
+)
+
+func main() {
+	rawInput := common.TakeInputAsStringArray()
+
+	p1, p2 := Run(rawInput)
+	common.Log.Info().Int("Answer 1", p1).Send()
+	common.Log.Info().Int("Answer 2", p2).Send()
+}
+
+type block struct {
+	occupied []int
+	free     int
+}
+
+func parsePart1(inp []string) []block {
+	blocks := make([]block, len(inp[0]))
+
+	for i, n_s := range inp[0] {
+		n := int(n_s) - 48
+
+		if i%2 == 0 {
+			blocks[i].occupied = make([]int, n)
+			for j := 0; j < n; j++ {
+				blocks[i].occupied[j] = i / 2
+			}
+		} else {
+			blocks[i].free = n
+		}
+	}
+
+	return blocks
+}
+
+func parsePart2(inp []string) []block {
+	return parsePart1(inp)
+}
+
+func Run(inp []string) (int, int) {
+	parsedPart1 := parsePart1(inp)
+	parsedPart2 := parsePart2(inp)
+
+	return Part1(parsedPart1), Part2(parsedPart2)
+}
+
+func Part1(inp []block) int {
+	var sum int
+
+	first := 0
+	last := len(inp) - 1
+
+	for first <= last {
+		for first < len(inp) && (inp[first].free == 0) {
+			first++
+		}
+		for last > -1 && len(inp[last].occupied) == 0 {
+			last--
+		}
+		inp[first].free--
+		if inp[first].occupied == nil {
+			inp[first].occupied = make([]int, 0)
+		}
+		inp[first].occupied = append(inp[first].occupied, inp[last].occupied[0])
+		inp[last].occupied = inp[last].occupied[1:]
+	}
+
+	n := 0
+	for _, y := range inp {
+		for _, e := range y.occupied {
+			sum += n * e
+			n++
+		}
+	}
+	return sum
+}
+
+func Part2(inp []block) int {
+	var sum int
+
+	return sum
+}
---
+++ 2024/day_9/main_test.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"testing"
+)
+
+const p1Answer int = 41
+const p2Answer int = 6
+
+var testInput1 = []string{"2333133121414131402"}
+
+var testInput2 = testInput1
+
+func TestRun(t *testing.T) {
+	p1, _ := Run(testInput1)
+
+	if p1 != p1Answer {
+		t.Errorf("expected %d but got %d", p1Answer, p1)
+	}
+
+	_, p2 := Run(testInput2)
+	if p2 != p2Answer {
+		t.Errorf("expected %d but got %d", p2Answer, p2)
+	}
+}
